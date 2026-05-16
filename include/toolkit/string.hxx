2a9b6b9ae330abe7a9fc1877fdca0dc1bd2c722f
Scriptor25 <f.schreiber.2006@gmail.com>
2026-05-16 15:02:34 +0200

    added string helpers

--- include/toolkit/string.hxx
+++ include/toolkit/string.hxx
@@ -19,6 +19,96 @@ namespace toolkit
             vec.push_back(str.substr(b, e - b));
     }
 
+    template<typename S, typename D>
+    std::vector<S> split(const S &str, D delim)
+    {
+        std::vector<S> vec;
+
+        size_t b{}, e{};
+        for (; (e = str.find(delim, b)) != S::npos; b = e)
+            if (b != e)
+                vec.push_back(str.substr(b, e - b));
+
+        if (b != e)
+            vec.push_back(str.substr(b, e - b));
+
+        return vec;
+    }
+
+    template<typename S, typename D>
+    void join(S &str, const std::vector<S> &vec, D delim)
+    {
+        str.clear();
+
+        for (auto it = vec.begin(); it != vec.end(); ++it)
+        {
+            if (it != vec.begin())
+                str += delim;
+
+            str += *it;
+        }
+    }
+
+    template<typename S, typename D>
+    S join(const std::vector<S> &vec, D delim)
+    {
+        S str;
+
+        for (auto it = vec.begin(); it != vec.end(); ++it)
+        {
+            if (it != vec.begin())
+                str += delim;
+
+            str += *it;
+        }
+
+        return str;
+    }
+
+    template<typename S>
+    void trim(S &dst, const S &src)
+    {
+        std::string::iterator begin, end;
+
+        for (auto it = src.begin(); it != src.end(); ++it)
+            if (*it > 0x20)
+            {
+                begin = it;
+                break;
+            }
+
+        for (auto it = src.rbegin(); it != src.rend(); ++it)
+            if (*it > 0x20)
+            {
+                end = it.base();
+                break;
+            }
+
+        dst = { begin, end };
+    }
+
+    template<typename S>
+    S trim(const S &src)
+    {
+        std::string::iterator begin, end;
+
+        for (auto it = src.begin(); it != src.end(); ++it)
+            if (*it > 0x20)
+            {
+                begin = it;
+                break;
+            }
+
+        for (auto it = src.rbegin(); it != src.rend(); ++it)
+            if (*it > 0x20)
+            {
+                end = it.base();
+                break;
+            }
+
+        return { begin, end };
+    }
+
     template<typename S>
     S lowercase(S str)
     {
