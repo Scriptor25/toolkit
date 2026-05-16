46dc137cac1f0a4ef188fc18b93a4dfb53fcc63d
Scriptor25 <f.schreiber.2006@gmail.com>
2026-05-16 10:49:59 +0200

    added defer helper

---
+++ include/toolkit/defer.hxx
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <functional>
+#include <tuple>
+
+namespace toolkit
+{
+    template<typename F, typename... A>
+    struct deferred
+    {
+        deferred(F &&f, A &&... a)
+            : f(std::forward<F>(f)),
+              a(std::forward<A>(a)...)
+        {
+        }
+
+        deferred(const deferred &) = delete;
+        deferred &operator=(const deferred &) = delete;
+
+        deferred(deferred &&other) noexcept
+            : f(std::move(other.f)),
+              a(std::move(other.a))
+        {
+        }
+
+        deferred &operator=(deferred &&other) noexcept
+        {
+            std::swap(f, other.f);
+            std::swap(a, other.a);
+
+            return *this;
+        }
+
+        ~deferred()
+        {
+            std::apply(f, a);
+        }
+
+        F f;
+        std::tuple<A...> a;
+    };
+
+    template<typename F, typename... A>
+    deferred<F, A...> defer(F &&f, A &&... a)
+    {
+        return { std::forward<F>(f), std::forward<A>(a)... };
+    }
+}
